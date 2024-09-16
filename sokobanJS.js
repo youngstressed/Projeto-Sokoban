009aab4c2bcfc164a3c83c20a65fa4d7746f195c
Gabriel <gabriel.sansao022@gmail.com>
2024-09-16 14:55:24 -0300

    Atualização no codigo 2.3

--- sokobanJS.js
+++ sokobanJS.js
@@ -1,15 +1,18 @@
-const tabuleiro = document.getElementById ('board2');
-const linha = document.createElement('div');
-linha.classList.add('row');
-
-for(i=0; i<4; i++) {
-const celula = document.createElement('div');
-celula.classList.add('cell');
-linha.append(celula);
-for(i=0; i<7; i++) {
-    const celula = document.createElement('div');
-    celula.classList.add('cell');
-    linha.append(celula); 
-}
-}
-tabuleiro.append(linha);
+const tabuleiro = document.getElementById('board2');
+ function criarlinha(linhas){
+         const linha = document.createElement('div');
+         linha.classList.add('row');
+        for(let i=0; i<linhas; i++) {
+            const celula = document.createElement('div');
+            celula.classList.add('cell');
+            linha.appendChild(celula);
+        }
+        return linha;
+     }
+    const numlinhas = 8;
+    const numcelulas = 8;
+    for(let i=0; i<numlinhas; i++){
+        const linha = criarlinha(numcelulas)
+        tabuleiro.append(linha);
+    }
+    
