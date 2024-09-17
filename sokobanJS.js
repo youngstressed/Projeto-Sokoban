3334f5281cff0b2b6b118fece5fe4a2fe5c62213
Gabriel <gabriel.sansao022@gmail.com>
2024-09-17 14:48:06 -0300

    Atualização antes de adicionar as regras

--- sokobanJS.js
+++ sokobanJS.js
@@ -1,18 +1,34 @@
-const tabuleiro = document.getElementById('board2');
- function criarlinha(linhas){
-         const linha = document.createElement('div');
-         linha.classList.add('row');
-        for(let i=0; i<linhas; i++) {
+function builGameBoard(linhas, celulas){
+    const game = document.getElementById('game');
+    const board = document.createElement('div');
+    board.classList.add('board');
+
+    for(let k = 0; k < linhas; k++){
+        const linha = document.createElement('div');
+        linha.classList.add('row');
+        board.append(linha);
+
+        for(let i = 0; i < celulas; i++) {
             const celula = document.createElement('div');
             celula.classList.add('cell');
-            linha.appendChild(celula);
+            linha.append(celula);
         }
-        return linha;
-     }
-    const numlinhas = 8;
-    const numcelulas = 8;
-    for(let i=0; i<numlinhas; i++){
-        const linha = criarlinha(numcelulas)
-        tabuleiro.append(linha);
-    }
-    
+    } 
+    game.append(board);
+}
+builGameBoard(8,8);
+
+
+
+     //  if(k > 0 && k < 7 && i > 0 && i < 7 ){
+            //   celula.classList.add('remove');
+            // }
+            // if (k == i) {
+            //     celula.classList.add('remove')
+            // }
+            // if (k + i == linhas -1) {
+            //     celula.classList.add('remove')
+            // }
+            // if (k == i || k + i == linhas -1) {
+            //     celula.classList.add('remove')
+            // }
