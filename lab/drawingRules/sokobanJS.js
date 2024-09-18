ab48da9487390b78271b164c65ac7e430b23854a
Gabriel <gabriel.sansao022@gmail.com>
2024-09-18 14:47:57 -0300

    Adicionamento das pastas drawing

---
+++ lab/drawingRules/sokobanJS.js
@@ -0,0 +1,50 @@
+buildGameBoard(8, 8, regra1);
+buildGameBoard(8, 8, regra2);
+buildGameBoard(8, 8, regra3);
+buildGameBoard(8, 8, regra4);
+
+function buildGameBoard(linhas, celulas, regras) {
+    const game = document.getElementById('game');
+    const board = document.createElement('div');
+    board.classList.add('board');
+
+    for (let k = 0; k < linhas; k++) {
+        const linha = document.createElement('div');
+        linha.classList.add('row');
+        board.append(linha);
+
+        for (let i = 0; i < celulas; i++) {
+            const celula = document.createElement('div');
+            celula.classList.add('cell');
+            linha.append(celula);
+
+            if (regras(linhas, celulas, k, i)){
+                celula.classList.add('remove');
+            }
+        }
+    }
+
+    game.append(board);
+}
+    
+
+    function regra1(linhas, celulas, k, i) {
+       return k > 0 && k < 7 && i > 0 && i < 7; 
+    }
+
+    function regra2(linhas, celulas, k, i) {
+        return k == i;
+        }
+
+    function regra3(linhas, celulas, k, i) {
+        return k + i == linhas - 1;
+    }
+
+    function regra4(linhas, celulas, k, i) {
+        return k == i || k + i == linhas - 1;
+    }
+
+    function regra5(linhas, celulas, k, i) {
+        return k == i;
+    }
+  
