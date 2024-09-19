dc754c879dfe2042f0de0f5f0d74c68320741896
Gabriel <gabriel.sansao022@gmail.com>
2024-09-19 17:39:54 -0300

    Adicionamento de movimento

--- sokobantest.js
+++ sokobantest.js
@@ -1,22 +1,29 @@
-function Jogador(posX, posY){
+function Jogador(posX, posY) {
     this.posX = posX;
     this.posY = posY;
 }
-const player = new Jogador(3, 4);
+const player = new Jogador(0, 0);
 
 const element = document.querySelector('.player');
 
 // player.addEventListener("click", function(){
 //     alert("Clicou no jogador");
 // })
-window.addEventListener("keydown", function(event){
-    nextPosition (event.code);
-})
- function nextPosition (keycode){
-if (keycode === 'ArrowUp') player.posX--;
-if (keycode === 'ArrowDown') player.posX++;
-if (keycode === 'ArrowLeft') player.posY--;
-if (keycode === 'ArrowRight') player.posY++;
+window.addEventListener("keydown", function (event) {
+    nextPosition(event.code);
 
-console.log(keycode,player);
-}
+    function nextPosition(keycode) {
+        if (keycode === 'ArrowUp') player.posX--;
+        if (keycode === 'ArrowDown') player.posX++;
+        if (keycode === 'ArrowLeft') player.posY--;
+        if (keycode === 'ArrowRight') player.posY++;
+
+        console.log(keycode, player);
+
+        const celulas = document.querySelectorAll('.cell');
+        const K = player.posX * 4 + player.posY;
+        console.log(celulas);
+
+        celulas[K].append(element);
+    }
+})
