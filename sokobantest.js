9978880c67e3986084538353474f1aef32c2ed2d
Gabriel <gabriel.sansao022@gmail.com>
2024-09-20 14:19:25 -0300

    Adicionamento da Função movePlayer

--- sokobantest.js
+++ sokobantest.js
@@ -13,17 +13,23 @@ window.addEventListener("keydown", function (event) {
     nextPosition(event.code);
 
     function nextPosition(keycode) {
-        if (keycode === 'ArrowUp') player.posX--;
-        if (keycode === 'ArrowDown') player.posX++;
-        if (keycode === 'ArrowLeft') player.posY--;
-        if (keycode === 'ArrowRight') player.posY++;
+        if (keycode === 'ArrowUp' && player.x) player.posX--;
+        if (keycode === 'ArrowDown' && player.x) player.posX++;
+        if (keycode === 'ArrowLeft' && player.y ) player.posY--;
+        if (keycode === 'ArrowRight' && player.y) player.posY++;
 
         console.log(keycode, player);
 
         const celulas = document.querySelectorAll('.cell');
-        const K = player.posX * 4 + player.posY;
-        console.log(celulas);
+        
 
         celulas[K].append(element);
+    }
+    function movePlayer(player){
+        const K = player * 4 + player;
+        console.log(celulas);
+    }
+    function verifyPosition() {
+
     }
 })
