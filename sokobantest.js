78e7ab96cc4bf0fa6025536d50d416cdc86e2fa6
Gabriel <gabriel.sansao022@gmail.com>
2024-09-24 17:29:51 -0300

    Otimização do codigo mudança no append.

--- sokobantest.js
+++ sokobantest.js
@@ -1,39 +1,11 @@
-buildGameBoard(10, 10, regra0);
-function buildGameBoard(linhas, celulas, regras) {
-    const game = document.getElementById('game');
-    const board = document.createElement('div');
-    const jogador = document.createElement('div');
+const DIST_SALTO = 66;
+const MARGIN_FIX = 4;
+const NUM_ROWS = 6;
+const NUM_COLS = 6;
+buildGameBoard(NUM_ROWS, NUM_COLS, regra0);
 
-
-    jogador.classList.add('player');
-    board.classList.add('board');
-
-    for (let k = 0; k < linhas; k++) {
-        const linha = document.createElement('div');
-        linha.classList.add('row');
-        board.append(linha);
-
-        for (let i = 0; i < celulas; i++) {
-            const celula = document.createElement('div');
-            celula.classList.add('cell');
-            linha.append(celula);
-
-            if (regras(linhas, celulas, k, i)) {
-                celula.classList.add('remove');
-            }
-        }
-    }
-
-    game.append(board);
-    board.append(jogador);
-}
-function regra0() {
-}
 const player = new Jogador(0, 0);
 const element = document.querySelector('.player');
-
-const dist_salto = 66;
-const margin_fix = 4;
 
 element.style.top = calculaPosicao(0);
 element.style.left = calculaPosicao(0);
@@ -72,9 +44,40 @@ function Jogador(posX, posY) {
 
 function verifyPosition(position) {
     let { x, y } = position;
-    return x >= 0 && x < 10 && y >= 0 && y < 10;
+    return x >= 0 && x < NUM_ROWS && y >= 0 && y < NUM_COLS;
 }
 
 function calculaPosicao(qtd) {
-    return qtd * dist_salto + margin_fix + "px";
+    return qtd * DIST_SALTO + MARGIN_FIX + "px";
 }
+
+function createGameElement(elementName, className, parentNode) {
+    const element = document.createElement(elementName)
+    element.classList.add(className);
+    parentNode.append(element);
+
+    return element;
+}
+
+function buildGameBoard(linhas, celulas, regras) {
+    const game = document.getElementById('game');
+
+    const board = createGameElement('div', 'board', game);
+    const jogador = createGameElement('div','player', board);
+    
+    for (let k = 0; k < linhas; k++) {
+        const linha = createGameElement('div','row', board);
+
+        for (let i = 0; i < celulas; i++) {
+            const celula = createGameElement('div','cell', linha);
+
+            if (regras(linhas, celulas, k, i)) {
+                celula.classList.add('remove');
+            }
+        }
+    }
+}
+function regra0() {
+}
+
+// functio
