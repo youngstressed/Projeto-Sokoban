11d388099a4e00d29ea606949bee594207ad98d7
Gabriel <gabriel.sansao022@gmail.com>
2024-09-24 15:58:20 -0300

    Atualização do codigo criando tabuleiro de forma dinâmica.

--- sokobantest.js
+++ sokobantest.js
@@ -1,9 +1,39 @@
+buildGameBoard(10, 10, regra0);
+function buildGameBoard(linhas, celulas, regras) {
+    const game = document.getElementById('game');
+    const board = document.createElement('div');
+    const jogador = document.createElement('div');
+    jogador.classList.add('player');
+
+    board.append(jogador);
+    board.classList.add('board');
+
+    for (let k = 0; k < linhas; k++) {
+        const linha = document.createElement('div');
+        linha.classList.add('row');
+        board.append(linha);
+
+        for (let i = 0; i < celulas; i++) {
+            const celula = document.createElement('div');
+            celula.classList.add('cell');
+            linha.append(celula);
+
+            if (regras(linhas, celulas, k, i)) {
+                celula.classList.add('remove');
+            }
+        }
+    }
+
+    game.append(board);
+}
+function regra0(k, i) {
+    return k == 0 || k == 7 || i == 0 || i == 7;
+}
 const player = new Jogador(0, 0);
-const celulas = document.querySelectorAll('.cell');
 const element = document.querySelector('.player');
 
 const dist_salto = 66;
-const margin_fix =  4;
+const margin_fix = 4;
 
 element.style.top = calculaPosicao(0);
 element.style.left = calculaPosicao(0);
@@ -11,9 +41,8 @@ element.style.left = calculaPosicao(0);
 window.addEventListener("keydown", function (event) {
     const next = player.nextPosition(event.code);
 
-    if (verifyPosition(next)){
-    let K = next.x * 4 + next.y;
-     player.moveTo(next, element, celulas[K]);
+    if (verifyPosition(next)) {
+        player.moveTo(next, element);
     }
 });
 
@@ -27,15 +56,15 @@ function Jogador(posX, posY) {
         if (keycode === 'ArrowDown') x++;
         if (keycode === 'ArrowLeft') y--;
         if (keycode === 'ArrowRight') y++;
-        
+
         console.log(keycode, player);
         return { x, y };
     }
 
-    this.moveTo = function (position, _parent) {
+    this.moveTo = function (position, element) {
         this.x = position.x;
         this.y = position.y;
-        
+
         element.style.top = calculaPosicao(this.x);
         element.style.left = calculaPosicao(this.y);
     }
@@ -43,9 +72,9 @@ function Jogador(posX, posY) {
 
 function verifyPosition(position) {
     let { x, y } = position;
-    return x >= 0 && x < 4 && y >= 0 && y < 4;
+    return x >= 0 && x < 10 && y >= 0 && y < 10;
 }
 
 function calculaPosicao(qtd) {
-    return qtd * dist_salto + margin_fix + "px";      
+    return qtd * dist_salto + margin_fix + "px";
 }
