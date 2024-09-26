0ec22554f855605162ec4058e2d98643cbd81516
Gabriel <gabriel.sansao022@gmail.com>
2024-09-26 19:05:27 -0300

    Adicionando a função "Piece" ao Piece.js

---
+++ piece.js
@@ -0,0 +1,30 @@
+
+
+function verifyPosition(position) {
+let { x, y } = position;
+return boardMap[x][y] != '#';
+}
+
+function Piece(posX, posY) {
+    this.x = posX;
+    this.y = posY;
+    this.nextPosition = function (keycode) {
+        let { x, y } = this;
+
+        if (keycode === 'ArrowUp') x--;
+        if (keycode === 'ArrowDown') x++;
+        if (keycode === 'ArrowLeft') y--;
+        if (keycode === 'ArrowRight') y++;
+
+        console.log(keycode, player);
+        return { x, y };
+    }
+
+    this.moveTo = function (position, element) {
+        this.x = position.x;
+        this.y = position.y;
+
+        element.style.top = calculaPosicao(this.x);
+        element.style.left = calculaPosicao(this.y);
+    }
+}
--- sokobantest.js
+++ sokobantest.js
@@ -19,37 +19,8 @@ window.addEventListener("keydown", function (event) {
     if (verifyPosition(next)) {
         player.moveTo(next, playerElement);
     }
-});
-
-function Piece(posX, posY) {
-    this.x = posX;
-    this.y = posY;
-    this.nextPosition = function (keycode) {
-        let { x, y } = this;
-
-        if (keycode === 'ArrowUp') x--;
-        if (keycode === 'ArrowDown') x++;
-        if (keycode === 'ArrowLeft') y--;
-        if (keycode === 'ArrowRight') y++;
-
-        console.log(keycode, player);
-        return { x, y };
-    }
-
-    this.moveTo = function (position, element) {
-        this.x = position.x;
-        this.y = position.y;
-
-        element.style.top = calculaPosicao(this.x);
-        element.style.left = calculaPosicao(this.y);
-    }
-}
-
-function verifyPosition(position) {
-    let { x, y } = position;
-    return boardMap[x][y] != '#';
-}
-
+    });
+    
 function calculaPosicao(qtd) {
     return qtd * DIST_SALTO + MARGIN_FIX + "px";
 }
