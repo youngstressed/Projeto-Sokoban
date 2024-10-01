a5c49a798ea6ee835618a52e2f1d4601d71357be
Gabriel <gabriel.sansao022@gmail.com>
2024-10-01 17:11:52 -0300

    Atualização no codigo... adicionamento das caixas e otimização do codigo.

--- piece.js
+++ piece.js
@@ -18,8 +18,7 @@ function Piece(posX, posY) {
         if (keycode === 'KeyS') y++;
         if (keycode === 'KeyA') x--;
         if (keycode === 'KeyD') x++;
-
-        console.log(keycode);
+        
         return { x, y };
     }
 
--- script.js
+++ script.js
@@ -3,28 +3,51 @@ import { buildGameBoard, boardMap } from "./board.js";
 
 const pieces = buildGameBoard();
 const board = document.querySelector('.board')
-console.log(pieces);
 
 const playerPiece = createBoardPiece(pieces.player, 'player');
 const boxes = [];
-    for(let box of pieces.boxes){
-        let piece = createBoardPiece(box, 'box'); 
-        boxes.push(piece);
-    };
+for (let box of pieces.boxes) {
+    let piece = createBoardPiece(box, 'box');
+    boxes.push(piece);
+};
 
-    window.addEventListener("keydown", function (event) {
-        handleKeyDownEvent(event.code);
-    });
+window.addEventListener("keydown", function (event) {
+    // event.preventDefault();
 
+    handlePieceMovement(event.code);
+});
+function findBoxAtPosition(position) {
+
+    return boxes.find((box) => box.x === position.x && box.y === position.y);
+}
+
+function handlePieceMovement(keycode) {
+
+    const next = playerPiece.nextPosition(keycode);
+
+    const foundBox = findBoxAtPosition(next);
+
+    if (foundBox) {
+
+        console.log(foundBox);
+
+    }
+    else {
+
+        if (verifyPosition(next)) {
+            playerPiece.moveTo(next);
+        }
+    }
+}
 function createBoardPiece(piecePosition, className) {
     const piece = new Piece(piecePosition.x, piecePosition.y);
     piece.insertElementInto(className, board);
-    
+
     return piece;
 
 }
 
-function handleKeyDownEvent(keycode){
+function handleKeyDownEvent(keycode) {
     const next = playerPiece.nextPosition(keycode);
 
     if (verifyPosition(next)) {
